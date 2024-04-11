cb2127a750f44e1474eebd744e2b3cd6327f3d48
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-04-11 14:36:20 +0800

    🤦‍♂️

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const PORT = 5555 || process.env.PORT;
 let app = express();
 //refeactor: check if methods and credentials are working even if they are not used
 app.use(cors({
-    origin: [process.env.APP_URL, "http://localhost"], 
+    origin: [process.env.APP_URL, "https://localhost"], 
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
